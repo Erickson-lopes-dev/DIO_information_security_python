836674d74ece019892934a47389c331f70e1d004
Erickson-lopes-dev <ofc.erickson@gmail.com>
2021-01-05 09:27:13 -0300

    web scraping

---
+++ 08_scrapper/web_scraping.py
@@ -0,0 +1,19 @@
+from bs4 import BeautifulSoup
+
+import requests
+
+# indica o site que deseja indicar
+site = requests.get("https://www.climatempo.com.br/").content
+
+# Esta BAIXANDO DO SITE  o conteudo html do site
+soup = BeautifulSoup(site, 'html.parser')
+
+# transforma em string para leitura em python
+# print(soup.prettify())
+
+temperatura = soup.find("span", class_="temperature _margin-l-5 -font-13")
+
+print(temperatura.string)
+print(temperatura.text)
+
+
