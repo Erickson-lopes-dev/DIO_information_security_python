48503ee2a726eb7b9d5ac67fed4153766ddd1951
Erickson-lopes-dev <ofc.erickson@gmail.com>
2021-01-05 09:42:42 -0300

    erro do 08 arrumadoi

--- 08_scrapper/web_scraping.py
+++ 08_scrapper/web_scraping.py
@@ -3,7 +3,7 @@ from bs4 import BeautifulSoup
 import requests
 
 # indica o site que deseja indicar
-site = requests.get("https://www.climatempo.com.br/").content
+site = requests.get("http://tempo.clic.com.br/sp/itapecerica-da-serra").content
 
 # Esta BAIXANDO DO SITE  o conteudo html do site
 soup = BeautifulSoup(site, 'html.parser')
@@ -11,17 +11,12 @@ soup = BeautifulSoup(site, 'html.parser')
 # transforma em string para leitura em python
 # print(soup.prettify())
 
-temperatura = soup.find("span", class_="temperature _margin-l-5 -font-13")
+temperatura = soup.find("span", class_="temperature_now")
 
 print(temperatura.text)
 
-print(soup.a)
-
-for item in soup.find_all('span'):
-    print(item.string)
 
 
-# SITE NAO ESTA MAIS IMPRIMINDO O TEMPO
 
 
 
